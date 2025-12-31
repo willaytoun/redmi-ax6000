2056a3fb4c0fd29857f295aab3e7975e81951dde
willaytoun <43229736+willaytoun@users.noreply.github.com>
2025-12-31 11:47:18 +0800

    Refactor Passwall feed addition in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+set -e
 
-# 添加 Passwall 官方软件源
+# Add Passwall feeds (avoid duplicate)
+grep -q passwall_packages feeds.conf.default || \
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+
+grep -q '^src-git passwall ' feeds.conf.default || \
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
